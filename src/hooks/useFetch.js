94701e711523a7b1f6a6e9d25014e908188b5c16
shay <shayforde@gmail.com>
2023-02-06 10:17:09 +0000

    final commit

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,16 +1,23 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
-export const useFetch = (url) => {
+export const useFetch = (url, _options) => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
 
+  // use useRef to wrap an object/array argument
+  // which is a useEffect dependency
+  const options = useRef(_options).current
+
   useEffect(() => {
+    console.log(options)
+    const controller = new AbortController()
+
     const fetchData = async () => {
       setIsPending(true)
 
       try {
-        const res = await fetch(url)
+        const res = await fetch(url, { signal: controller.signal })
         if (!res.ok) {
           throw new Error(res.statusText)
         }
@@ -20,14 +27,21 @@ export const useFetch = (url) => {
         setData(data)
         setError(null)
       } catch (err) {
-        setIsPending(false)
-        setError("Could not fetch the data")
-        console.log(err.message)
+        if (err.name === "AbortError") {
+          console.log("the fetch was aborted")
+        } else {
+          setIsPending(false)
+          setError("Could not fetch the data")
+        }
       }
     }
 
     fetchData()
-  }, [url])
+
+    return () => {
+      controller.abort()
+    }
+  }, [url, options])
 
   return { data, isPending, error }
 }
